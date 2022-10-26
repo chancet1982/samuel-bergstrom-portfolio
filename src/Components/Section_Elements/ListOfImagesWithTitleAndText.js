1824eb6d411de522161e63e4d4ff936a19ab7fbd
chancet1982 <chancet1982@gmail.com>
2022-10-26 09:07:36 +0200

    doing the apple thing on listofimageswithtitleandtext

--- src/Components/Section_Elements/ListOfImagesWithTitleAndText.js
+++ src/Components/Section_Elements/ListOfImagesWithTitleAndText.js
@@ -2,71 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import ImageWithTitleAndText from "../Shared/ImageWithTitleAndText";
-import padding from "../../theme/padding";
-import breakpoints from "../../theme/breakpoints";
-import sizes from "../../theme/sizes";
+import ImageWithTitleAndText2 from "../Shared/ImageWithTitleAndText2";
 
 const StyledListOfImagesWithTitleAndText = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
-
-  > div {
-    overflow: hidden;
-    display: grid;
-    grid-template-columns: repeat(1, 1fr);
-
-    > figure {
-      @media (max-width: ${breakpoints.desktop - 1}px) {
-        padding-left: ${padding.horizontal.double};
-        padding-right: ${padding.horizontal.double};
-      }
-    }
-
-    @media (min-width: ${breakpoints.desktop}px) {
-      max-width: ${sizes.contentWidthLimit}px;
-      margin: 0 auto;
-    }
-  }
-
-  > div:nth-of-type(2n + 1) {
-    @media (min-width: ${breakpoints.desktop}px) {
-      grid-template-columns: repeat(2, 1fr);
-      grid-template-areas: "a b";
-
-      > figure {
-        grid-area: a;
-        padding-left: ${padding.horizontal.double};
-      }
-
-      > div {
-        grid-area: b;
-      }
-    }
-  }
-
-  > div:nth-of-type(2n + 2) {
-    @media (min-width: ${breakpoints.desktop}px) {
-      grid-template-columns: repeat(2, 1fr);
-      grid-template-areas: "a b";
-
-      > figure {
-        grid-area: b;
-        padding-right: ${padding.horizontal.double};
-      }
-
-      > div {
-        grid-area: a;
-      }
-    }
-  }
 `;
 
+/* TODO: After changes to list of images with title and text we will need to change the sneakpeak images on all cases. */
 function ListOfImagesWithTitleAndText({ items }) {
   return (
     <StyledListOfImagesWithTitleAndText>
       {items.map(({ imageUrl, imageAlt, title, text }, index) => (
-        <ImageWithTitleAndText
+        <ImageWithTitleAndText2
           imageUrl={imageUrl}
           imageAlt={imageAlt}
           title={title}
---
+++ src/Components/Shared/ImageWithTitleAndText2.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-nested-ternary */
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import Title from "./Title";
+
+const StyledTitleAndText = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 80vh;
+  box-sizing: border-box;
+  grid-area: ${({ $flip }) => ($flip ? "b" : "a")};
+`;
+
+const StyledImageWithTitleAndText = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 50%);
+  max-width: 1280px;
+  margin: 0 auto;
+  grid-template-areas: "a b";
+`;
+
+const StyledImage = styled(motion.div)`
+  height: 80vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 50%;
+  position: absolute;
+
+  ${({ $imageUrl }) =>
+    $imageUrl && {
+      backgroundImage: `url(/${$imageUrl})`,
+    }}
+
+  background-position: ${({ $flip }) => ($flip ? "right" : "left")};
+  left: ${({ $flip }) => ($flip ? "0" : "auto")};
+  right: ${({ $flip }) => ($flip ? "auto" : "0")};
+`;
+
+/* TODO: StyledTitleAndText doesnt get props (for whatever reason) */
+function ImageWithTitleAndText({ imageUrl, title, text, flip }) {
+  return (
+    <StyledImageWithTitleAndText
+      initial="hidden"
+      whileInView="inView"
+      viewport={{ once: true, amount: 0.2 }}
+      transition={{ staggerChildren: 0.2 }}
+    >
+      {flip ? (
+        <>
+          <StyledImage $imageUrl={imageUrl} $flip />
+          <StyledTitleAndText isPadded style={{ gridArea: "b" }}>
+            <Title h={3}>{title}</Title>
+            {text}
+          </StyledTitleAndText>
+        </>
+      ) : (
+        <>
+          <StyledTitleAndText isPadded>
+            <Title h={3}>{title}</Title>
+            {text}
+          </StyledTitleAndText>
+          <StyledImage $imageUrl={imageUrl} />
+        </>
+      )}
+    </StyledImageWithTitleAndText>
+  );
+}
+
+ImageWithTitleAndText.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  text: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  flip: PropTypes.bool,
+};
+
+ImageWithTitleAndText.defaultProps = {
+  flip: false,
+};
+
+export default ImageWithTitleAndText;
