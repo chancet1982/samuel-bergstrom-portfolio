a6bd7325e94b706e96e7ca0a5542c1f4df4441ae
chancet1982 <chancet1982@gmail.com>
2022-11-30 20:53:12 +0100

    Finally cracked it!

--- src/Components/Section_Elements/ListOfCards.js
+++ src/Components/Section_Elements/ListOfCards.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { useWindowSize } from "react-use";
+import { useWindowSize, useSize } from "react-use";
 import Card from "./ListOfCards/Card";
 import breakpoints from "../../theme/breakpoints";
 import sizes from "../../theme/sizes";
 import padding from "../../theme/padding";
 
 const StyledListOfCards = styled(motion.div)`
+  position: relative;
+  padding: 0 4vw;
+
   @media (min-width: ${breakpoints.desktop}px) {
     max-width: ${sizes.contentWidthLimit}px;
     margin: 0 auto;
@@ -21,6 +24,8 @@ const StyledList = styled(motion.ul)`
   padding: 0;
   display: flex;
   width: max-content;
+  padding-left: ${padding.horizontal.double};
+  padding-right: ${padding.horizontal.double};
 `;
 
 const StyledListItem = styled(motion.li)`
@@ -40,35 +45,29 @@ const StyledListItem = styled(motion.li)`
 `;
 
 const StyledListNavigationButtons = styled(motion.div)``;
-const StyledListButton = styled(motion.button)``;
+const StyledListButton = styled(motion.button)`
+  padding: 1rem 1.5rem;
+`;
 /* TODO: add side scrolling with Framer motion https://codesandbox.io/s/framer-motion-2-scrollable-drag-constraints-lsonq?file=/src/App.js */
-/* TODO: Make sure card animation is animated  */
-/* TODO: Make sure that when "Next" card is pressed it is moving things it never goes outside available range. */
 /* TODO: replace listWidth magic number with useMeasrure */
 /* TODO: Make sure number of columns changes for mobile (1) table (2) and desktop (3) */
-/* TODO: Add left adn right gutters to the cardlist */
 /* TODO: Update design for list navigation buttons  */
 
 function ListOfCards({ items }) {
   const numberOfColumns = 3; // Update to work on mobile as well
   const [listOffset, setListOffset] = useState(0);
 
-  const { width } = useWindowSize();
-  const cardMarginRight = (width / 100) * 2; // Should be equal to 2VWs
+  const windowSize = useWindowSize();
+  const cardMarginRight = (windowSize.width / 100) * 2; // Should be equal to 2VWs
   const listItemWidth =
     (sizes.contentWidthLimit - cardMarginRight * (numberOfColumns - 1)) /
     numberOfColumns;
-  const listWidth = 2611.13; // Replace magic number with a better solution
-  const initialListOffset = (width - sizes.contentWidthLimit) / 2;
-  const availableRange = listWidth + initialListOffset - width;
+  const initialListOffset = (windowSize.width - sizes.contentWidthLimit) / 2;
 
-  const moveList = (offset) => {
-    setListOffset(listOffset + offset);
-  };
-
-  return (
-    <StyledListOfCards>
-      <StyledList style={{ x: listOffset }}>
+  const [styledList, { width }] = useSize(
+    // eslint-disable-next-line no-shadow, no-unused-vars
+    ({ width }) => (
+      <StyledList animate={{ x: listOffset }}>
         {items.map(({ mediaUrl, title, text }) => (
           <StyledListItem $listItemWidth={`${listItemWidth}px`}>
             <Card
@@ -80,15 +79,41 @@ function ListOfCards({ items }) {
           </StyledListItem>
         ))}
       </StyledList>
+    ),
+    { width: 0 }
+  );
+
+  const listWidth = width;
+  const availableRange = listWidth + initialListOffset - windowSize.width;
+
+  const moveList = (offset) => {
+    setListOffset(listOffset + offset);
+  };
+
+  return (
+    <StyledListOfCards>
+      {styledList}
       <StyledListNavigationButtons>
         <StyledListButton
-          onClick={() => moveList(listItemWidth)}
+          onClick={() =>
+            moveList(
+              -(listOffset + initialListOffset) <= listItemWidth
+                ? -(listOffset + initialListOffset)
+                : listItemWidth
+            )
+          }
           disabled={initialListOffset + listOffset >= 0}
         >
           Previous
         </StyledListButton>
         <StyledListButton
-          onClick={() => moveList(-listItemWidth)}
+          onClick={() =>
+            moveList(
+              availableRange + listOffset <= listItemWidth
+                ? -(availableRange + listOffset)
+                : -listItemWidth
+            )
+          }
           disabled={availableRange + listOffset <= 0}
         >
           Next
