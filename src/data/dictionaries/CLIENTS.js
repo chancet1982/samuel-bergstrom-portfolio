8e76570d4c8f2e2a31cc0ba26406e3cde8eccd30
chancet1982 <chancet1982@gmail.com>
2023-01-28 15:23:26 +0100

    fixing client images in about page

--- src/data/dictionaries/CLIENTS.js
+++ src/data/dictionaries/CLIENTS.js
@@ -1,111 +1,111 @@
 /* eslint-disable import/prefer-default-export */
 export const CLIENTS = [
   {
-    imageUrl: "assets/clients/color/client-26.svg",
-    imageAlt: "Clas Ohlson",
+    imageUrl: "assets/clients/color/qlick-color.svg",
+    imageAlt: "qlick",
   },
   {
-    imageUrl: "assets/clients/color/client-25.svg",
-    imageAlt: "NetSet",
+    imageUrl: "assets/clients/color/capish-color.svg",
+    imageAlt: "capish",
   },
   {
-    imageUrl: "assets/clients/color/client-24.svg",
-    imageAlt: "Euro Finans",
+    imageUrl: "assets/clients/color/diaverum-color.svg",
+    imageAlt: "diaverum",
   },
   {
-    imageUrl: "assets/clients/color/client-23.svg",
-    imageAlt: "E-on",
+    imageUrl: "assets/clients/color/nasdaq-omx-color.svg",
+    imageAlt: "nasdaq-omx",
   },
   {
-    imageUrl: "assets/clients/color/client-22.svg",
-    imageAlt: "Prisjakt",
+    imageUrl: "assets/clients/color/1337works-color.svg",
+    imageAlt: "1337works",
   },
   {
-    imageUrl: "assets/clients/color/client-00.svg",
-    imageAlt: "Mitsubishi Electric",
+    imageUrl: "assets/clients/color/clas-ohlson-color.svg",
+    imageAlt: "clas-ohlson",
   },
   {
-    imageUrl: "assets/clients/color/client-01.svg",
-    imageAlt: "Nasdaq OMX Nordic",
+    imageUrl: "assets/clients/color/euronext-color.svg",
+    imageAlt: "irish stock exchange",
   },
   {
-    imageUrl: "assets/clients/color/client-17.svg",
-    imageAlt: "Fitness 24/7",
+    imageUrl: "assets/clients/color/fitness24seven-color.svg",
+    imageAlt: "fitness24seven",
   },
   {
-    imageUrl: "assets/clients/color/client-04.svg",
-    imageAlt: "Qlik",
+    imageUrl: "assets/clients/color/nowaste-logistics-color.svg",
+    imageAlt: "nowaste-logistics",
   },
   {
-    imageUrl: "assets/clients/color/client-05.svg",
-    imageAlt: "ATEA",
+    imageUrl: "assets/clients/color/loomis-color.svg",
+    imageAlt: "loomis",
   },
   {
-    imageUrl: "assets/clients/color/client-18.svg",
-    imageAlt: "Alfa Laval",
+    imageUrl: "assets/clients/color/mitsubishi-electric-color.svg",
+    imageAlt: "mitsubishi-electric",
   },
   {
-    imageUrl: "assets/clients/color/client-14.svg",
-    imageAlt: "SEB",
+    imageUrl: "assets/clients/color/lumito-color.svg",
+    imageAlt: "lumito",
   },
   {
-    imageUrl: "assets/clients/color/client-15.svg",
-    imageAlt: "Loomis",
+    imageUrl: "assets/clients/color/euro-finans-color.svg",
+    imageAlt: "euro-finans",
   },
   {
-    imageUrl: "assets/clients/color/client-08.svg",
-    imageAlt: "Nordea",
+    imageUrl: "assets/clients/color/nordea-color.svg",
+    imageAlt: "nordea",
   },
   {
-    imageUrl: "assets/clients/color/client-09.svg",
-    imageAlt: "Diaverum",
+    imageUrl: "assets/clients/color/atea-color.svg",
+    imageAlt: "atea",
   },
   {
-    imageUrl: "assets/clients/color/client-12.svg",
-    imageAlt: "Lord Abbett",
+    imageUrl: "assets/clients/color/seb-color.svg",
+    imageAlt: "seb",
   },
   {
-    imageUrl: "assets/clients/color/client-07.svg",
-    imageAlt: "Capish",
-  },
-  {
-    imageUrl: "assets/clients/color/client-10.svg",
-    imageAlt: "Lumito",
-  },
-  {
-    imageUrl: "assets/clients/color/client-11.svg",
-    imageAlt: "Modity",
-  },
-  {
-    imageUrl: "assets/clients/color/client-03.svg",
-    imageAlt: "PFA Pension",
-  },
-  {
-    imageUrl: "assets/clients/color/client-13.svg",
-    imageAlt: "1337Works",
-  },
-  {
-    imageUrl: "assets/clients/color/client-02.svg",
-    imageAlt: "NoWaste Logistics",
-  },
-  /* {
-    imageUrl: "assets/clients/color/client-19.svg",
-    imageAlt: "Clas Ohlson",
-  },
-  {
-    imageUrl: "assets/clients/color/client-20.svg",
-    imageAlt: "Paradox Interactive",
-  },
-  {
-    imageUrl: "assets/clients/color/client-21.svg",
-    imageAlt: "Ikano Bank",
-  },
-  {
-    imageUrl: "assets/clients/color/client-16.svg",
-    imageAlt: "SilverRail",
-  }, 
-   {
-    imageUrl: "assets/clients/color/client-06.svg",
+    imageUrl: "assets/clients/color/tretton37-color.svg",
     imageAlt: "tretton37",
-  }, */
+  },
+  {
+    imageUrl: "assets/clients/color/lord-abbett-color.svg",
+    imageAlt: "lord-abbett",
+  },
+  {
+    imageUrl: "assets/clients/color/modity-color.svg",
+    imageAlt: "modity",
+  },
+  {
+    imageUrl: "assets/clients/color/pfa-color.svg",
+    imageAlt: "pfa",
+  },
+  {
+    imageUrl: "assets/clients/color/alfa-laval-color.svg",
+    imageAlt: "alfa-laval",
+  },
+  {
+    imageUrl: "assets/clients/color/netset-color.svg",
+    imageAlt: "netset",
+  },
+  {
+    imageUrl: "assets/clients/color/svensk-cater-color.svg",
+    imageAlt: "svensk-cater",
+  },
+  {
+    imageUrl: "assets/clients/color/be-at-your-best-color.svg",
+    imageAlt: "be-at-your-best",
+  },
+  {
+    imageUrl: "assets/clients/color/prisjakt-color.svg",
+    imageAlt: "prisjakt",
+  },
+  {
+    imageUrl: "assets/clients/color/sbab-color.svg",
+    imageAlt: "sbab",
+  },
+  {
+    imageUrl: "assets/clients/color/lu-innovation-color.svg",
+    imageAlt: "lu-innovation",
+  },
 ];
